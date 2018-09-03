386f9c7f5de0649abdb3dad2059dc26064f72f5e
lasseclaes <lasseclaes@gmail.com>
2018-09-03 13:24:18 +0200

    start

---
+++ js/laja-shake_v3.js
@@ -0,0 +1,30 @@
+//anvender https://github.com/alexgibson/shake.js til at lytte efter shake-eventet
+window.onload = function() {
+    document.getElementById("dev-1").innerHTML = "V. 3";
+    //create a new instance of shake.js.
+    var myShakeEvent = new Shake({
+        threshold: 15
+    });
+
+    // start listening to device motion
+    myShakeEvent.start();
+
+    // register a shake event
+    window.addEventListener('shake', shakeEventDidOccur, false);
+
+    //shake event callback
+    function shakeEventDidOccur () {
+      var which = Math.floor(Math.random() * 3) + 1;
+      var ssp = "";
+      if (which === 1){
+          ssp = "sten";
+        }
+      else if (which === 2){
+          ssp = "saks";
+        }
+      else{
+          ssp = "papir";
+        }
+      alert (ssp);
+    }
+};
